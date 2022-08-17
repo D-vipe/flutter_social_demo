f821b2a7baa46900f481cfffa97f79145582de80
Alexander <d-vipe@yandex.ru>
2022-08-17 19:55:38 +0300

    fix midleware behavior

--- lib/redux/middleware/user_middleware.dart
+++ lib/redux/middleware/user_middleware.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_social_demo/api/user_api.dart';
+import 'package:flutter_social_demo/app/config/exceptions.dart';
 import 'package:flutter_social_demo/app/constants/errors_const.dart';
 import 'package:flutter_social_demo/models/models.dart';
 import 'package:flutter_social_demo/redux/actions/user_actions.dart';
@@ -12,23 +12,22 @@ final UserApi _userApi = UserApi();
 
 List<Middleware<AppState>> createUsersListMiddleware() {
   return [
-    TypedMiddleware<AppState, GetUsersListAction>(
-        _fetchUsersList(refresh: false)),
-    TypedMiddleware<AppState, RefreshUsersListAction>(
-        _fetchUsersList(refresh: true)),
+    TypedMiddleware<AppState, GetUsersListAction>(_fetchUsersList(refresh: false)),
+    TypedMiddleware<AppState, RefreshUsersListAction>(_fetchUsersList(refresh: true)),
     TypedMiddleware<AppState, LoadMoreUsersAction>(_loadMoreUsers()),
   ];
 }
 
 Middleware<AppState> _fetchUsersList({required bool refresh}) {
   return (Store<AppState> store, action, NextDispatcher next) {
+    next(action);
+
     // Use caching service to reduce amount of server-requests
     List<User> data = [];
     bool needToFetch = true;
 
     // check caching time
-    if (!CachingService.needToSendRequest(
-        key: PreferenceKey.userListCacheTime)) {
+    if (!CachingService.needToSendRequest(key: PreferenceKey.userListCacheTime)) {
       // check if Hive is not empty
       data = CachingService.getCachedUsers();
       needToFetch = data.isEmpty;
@@ -36,35 +35,46 @@ Middleware<AppState> _fetchUsersList({required bool refresh}) {
 
     if (needToFetch) {
       // cache time and users list
-      _userApi.getList().then((List<User> users) {
-        CachingService.cacheUserList(list: users);
-        CachingService.setCachingTime(
-            key: PreferenceKey.userListCacheTime, time: DateTime.now());
-        store.dispatch(GetUsersListSucceedAction(usersList: users));
-      }).onError((error, _) => refresh
-          ? store.dispatch(GetUsersListSucceedAction(
-              usersList: store.state.usersListState.usersList ?? []))
-          : store.dispatch(GetUsersListErrorAction(GeneralErrors.emptyData)));
+      _userApi
+          .getList()
+          .then((List<User> users) {
+            CachingService.cacheUserList(list: users);
+            CachingService.setCachingTime(key: PreferenceKey.userListCacheTime, time: DateTime.now());
+            store.dispatch(GetUsersListSucceedAction(usersList: users));
+          })
+          .catchError(
+            test: ((error) => error is ParseException),
+            (error, _) => refresh
+                ? store.dispatch(GetUsersListSucceedAction(usersList: store.state.usersListState.usersList ?? []))
+                : store.dispatch(GetUsersListErrorAction(GeneralErrors.emptyData)),
+          )
+          .onError(
+            (error, _) => refresh
+                ? store.dispatch(GetUsersListSucceedAction(usersList: store.state.usersListState.usersList ?? []))
+                : store.dispatch(GetUsersListErrorAction(GeneralErrors.emptyData)),
+          );
     } else {
-      Future.delayed(const Duration(milliseconds: 100)).then((value) {
-        refresh
-            ? store.dispatch(RefreshUsersSucceedAction(usersList: data))
-            : store.dispatch(GetUsersListSucceedAction(usersList: data));
-      });
+      refresh ? store.dispatch(RefreshUsersSucceedAction(usersList: data)) : store.dispatch(GetUsersListSucceedAction(usersList: data));
     }
-    next(action);
   };
 }
 
 // Fake load more as json service return all available users at once
 Middleware<AppState> _loadMoreUsers() {
-  // TODO imitate page increment upon loading
   return (Store<AppState> store, action, NextDispatcher next) {
-    _userApi.getList().then((List<User> users) {
-      store.dispatch(GetMoreUsersSucceedAction(usersList: users));
-    }).onError((error, _) =>
-        store.dispatch(GetUsersListErrorAction(GeneralErrors.emptyData)));
-
     next(action);
+
+    _userApi
+        .getList()
+        .then((List<User> users) {
+          store.dispatch(GetMoreUsersSucceedAction(usersList: users));
+        })
+        .catchError(
+          test: ((error) => error is ParseException),
+          (error, _) => store.dispatch(GetMoreUsersSucceedAction(usersList: store.state.usersListState.usersList ?? [])),
+        )
+        .onError(
+          (error, _) => store.dispatch(GetUsersListErrorAction(GeneralErrors.emptyData)),
+        );
   };
 }
